65e27b40f98dce771df36a2778f51d0d0d6d2063
jsonmanojkumar <jsonmanojkumar@gmail.com>
2020-03-31 00:42:23 -0700

    first

---
+++ Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        string barCode = TextBox1.Text;
+        System.Web.UI.WebControls.Image imgBarCode = new System.Web.UI.WebControls.Image();
+        using (Bitmap bitMap = new Bitmap(barCode.Length * 40, 80))
+        {
+            using (Graphics graphics = Graphics.FromImage(bitMap))
+            {
+                Font oFont = new Font("IDAutomationHC39M", 16);
+                PointF point = new PointF(2f, 2f);
+                SolidBrush blackBrush = new SolidBrush(Color.Black);
+                SolidBrush whiteBrush = new SolidBrush(Color.White);
+                graphics.FillRectangle(whiteBrush, 0, 0, bitMap.Width, bitMap.Height);
+                graphics.DrawString("*" + barCode + "*", oFont, blackBrush, point);
+            }
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitMap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                byte[] byteImage = ms.ToArray();
+
+                Convert.ToBase64String(byteImage);
+                imgBarCode.ImageUrl = "data:image/png;base64," + Convert.ToBase64String(byteImage);
+            }
+            PlaceHolder1.Controls.Add(imgBarCode);
+        }
+        //Label3.Visible = true;
+
+    }
+}
